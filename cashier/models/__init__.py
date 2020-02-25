ef1e99657ec3e9ddc6cf934ce931d361e54d316f
SheehanWidodo <sheehanwidodo@gitlab.com>
2020-02-25 22:01:58 +0700

    Finishing Model

--- cashier/models/__init__.py
+++ cashier/models/__init__.py
@@ -1,3 +1,4 @@
 """Store models."""
-from .product import Product, ProductCategory
+from .product import Product, ProductCategory, HargaBertingkat, ConvertBarang
 from .user import User
+from .pembayaran import Pembayaran, PembayaranProduct

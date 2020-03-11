be75f6564019fbd77cd56ffb9c6ebb4894ed7b13
Josh Yehezkiel <yehezkiel@gmail.com>
2020-03-11 18:58:31 +0700

    Sales page 50%

--- cashier/serializers/sale.py
+++ cashier/serializers/sale.py
@@ -6,8 +6,12 @@ from cashier.models import Sale
 
 class SaleSerializer(serializers.ModelSerializer):
     """Sale Serializer."""
+    barcode = serializers.CharField(source='product.barcode', read_only=True)
+    product = serializers.CharField(source='product.name', read_only=True)
+    price = serializers.CharField(source='product.selling_price', read_only=True)
 
     class Meta:  # noqa D106
         model = Sale
         name = 'sale'
-        fields = '__all__'
+        fields = ['id', 'barcode', 'invoice', 'product', 'price', 'qty', 'total']
+        datatables_always_serialize = ('id', 'invoice', 'product', 'qty', 'total')
--- cashier/views/apis/sale.py
+++ cashier/views/apis/sale.py
@@ -1,5 +1,5 @@
 """Product Api view."""
-from cashier.models import Pembayaran, Invoice, Product, Sale
+from cashier.models import Invoice, Product, Sale
 from cashier.serializers.sale import SaleSerializer
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -10,7 +10,7 @@ from django.forms.models import model_to_dict
 class SaleViewSet(viewsets.ModelViewSet):
     """ProductViewSet."""
     serializer_class = SaleSerializer
-    queryset = Pembayaran.objects.order_by('created_at')
+    queryset = Sale.objects.order_by('created_at')
 
     @action(detail=False, methods=['POST'])
     def add_item(self, request):
@@ -23,22 +23,60 @@ class SaleViewSet(viewsets.ModelViewSet):
         invoice = Invoice.objects.get(invoice=invoice_number)
         product = Product.objects.get(barcode=barcode)
 
-        save = Sale.objects.create(invoice=invoice, product=product, qty=qty, total=total)
+        try:
+            product.stock = product.stock - int(qty)
+            product.save(update_fields=["stock"])
+        except Exception as e:
+            print(e)
 
-        return Response(model_to_dict(save))
+        try:
+            sale_item = Sale.objects.get(invoice=invoice, product=product)
+            new_qty = int(sale_item.qty) + int(qty)
+            new_total = int(sale_item.total) + int(total)
+            sale_item.qty = new_qty
+            sale_item.total = new_total
+            sale_item.save(update_fields=['qty', 'total'])
+        except Exception as e:
+            print(e)
+            sale_item = Sale.objects.create(invoice=invoice, product=product, qty=qty, total=total)
 
+        return Response(model_to_dict(sale_item))
 
     @action(detail=False, methods=['POST'])
     def get_by_invoice(self, request):
         """get_by_invoice."""
         invoice_number = request.POST.get('invoice_number')
-        barcode = request.POST.get('barcode')
-        qty = request.POST.get('qty')
+        invoice = Invoice.objects.get(invoice=invoice_number)
+        queryset = self.get_queryset().filter(invoice=invoice)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['POST'])
+    def process_payment(self, request):
+        """get_by_invoice."""
+        invoice_number = request.POST.get('invoice_number')
+        cash = request.POST.get('cash')
+        change = request.POST.get('change')
         total = request.POST.get('total')
+
+        invoice = Invoice.objects.get(invoice=invoice_number)
+        invoice.cash = cash
+        invoice.change = change
+        invoice.total = total
+        invoice.status = 1
+        invoice.save(update_fields=["cash", "change", "total", "status"])
+
+        return Response(invoice)
+
+    @action(detail=False, methods=['POST'])
+    def delete_item(self, request):
+        """delete_item."""
+        invoice_number = request.POST.get('invoice_number')
+        barcode = request.POST.get('barcode')
 
         invoice = Invoice.objects.get(invoice=invoice_number)
         product = Product.objects.get(barcode=barcode)
 
-        save = Sale.objects.create(invoice=invoice, product=product, qty=qty, total=total)
-
-        return Response(model_to_dict(save))
+        item = Sale.objects.get(invoice=invoice, product=product)
+        item.delete()
+        return Response(model_to_dict(item))

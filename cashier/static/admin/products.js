9f951799c1dbc1c2d7f7c5d0f1f8f75a1c61ccb5
JoshAbi Studio <yehezkieljosh@gmail.com>
2020-07-20 16:57:04 +0700

    fix product row id and sale template

--- cashier/static/admin/products.js
+++ cashier/static/admin/products.js
@@ -69,6 +69,7 @@ var KTDatatablesDataSourceAjaxServer = function() {
                     title: 'Actions',
                     orderable: false,
                     render: function(data, type, row) {
+                        console.log(row)
                         return `<a href="products/${row.id}" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Variant List">
                           <i class="nav-icon fas fa-edit"></i>
                     </a>`;

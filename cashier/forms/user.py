3391c73bfefd3382c527c6a6fc6a7e61598e93b4
JoshAbi Studio <yehezkieljosh@gmail.com>
2020-07-21 10:59:14 +0700

    Add auth backend for username login

--- cashier/forms/user.py
+++ cashier/forms/user.py
@@ -11,21 +11,21 @@ User = get_user_model()
 class DashUserCreationForm(CustomUserCreationForm):
     """Custom user creation form."""
 
-    def save(self, *args, **kwargs):
-        """Override save."""
-        self.instance.username = self.instance.email
-        return super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     """Override save."""
+    #     self.instance.username = self.instance.email
+    #     return super().save(*args, **kwargs)
 
     class Meta:  # noqa D106
         model = User
-        fields = ('email', 'password1', 'password2', )
+        fields = ('email', 'username', 'password1', 'password2', )
 
 
 class DashUserUpdateForm(CustomModelForm):
     """CmsUserUpdateForm."""
     class Meta:  # noqa D106
         model = User
-        fields = ('email', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active', )
+        fields = ('email', 'username', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active', )
         # widgets = {
         #     'picture': MetronicAvatar(),
         # }
